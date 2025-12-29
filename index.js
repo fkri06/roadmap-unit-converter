cc9180cc7e022c3827be8556d9fd0a71e0474a14
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-12-29 13:36:46 +0700

    add length unit factors

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const unitValue = document.getElementById("unitValue");
 const unitFrom = document.getElementById("unitFrom");
 const unitTo = document.getElementById("unitTo");
 const button = document.getElementById("button");
+const msg = document.getElementById("msg");
+
+const unitFactors = {
+    "millimeter": 0.001,
+    "centimeter": 0.01,
+    "meter": 1,
+    "kilometer": 1000,
+    "inch": 0.0254,
+    "foot": 0.3048,
+    "yard": 0.9144,
+    "mile": 1609.344
+}
+
