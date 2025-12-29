872aa2ebf908fc901f2881972a5a1a3a704cbfa7
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-12-29 11:32:06 +0700

    get the DOM objects for input tags and a button

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const myButton = document.getElementById("myButton");
-
-const colors = ["pink", "lightgreen", "cyan"]
-
-myButton.addEventListener("click", () => {
-    const getRandomColor = colors[Math.floor(Math.random() * 3)];
-    document.body.style.backgroundColor = `${getRandomColor}`;
-})
+const unitValue = document.getElementById("unitValue");
+const unitFrom = document.getElementById("unitFrom");
+const unitTo = document.getElementById("unitTo");
+const button = document.getElementById("button");
