17d9917af70784fb40c5e4d30ad4616c91903a69
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-12-30 09:43:23 +0700

    calculate the conversion and display it during click event on convert button

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const unitValue = document.getElementById("unitValue");
 const unitFrom = document.getElementById("unitFrom");
 const unitTo = document.getElementById("unitTo");
-const button = document.getElementById("button");
+const convertButton = document.getElementById("button");
 const msg = document.getElementById("msg");
 
 const unitFactors = {
-    "millimeter": 0.001,
-    "centimeter": 0.01,
-    "meter": 1,
-    "kilometer": 1000,
-    "inch": 0.0254,
-    "foot": 0.3048,
-    "yard": 0.9144,
+    "mm": 0.001,
+    "cm": 0.01,
+    "m": 1,
+    "km": 1000,
+    "in": 0.0254,
+    "ft": 0.3048,
+    "yd": 0.9144,
     "mile": 1609.344
 }
 
-button.addEventListener("click", () => {
-    if (unitValue.value === "" || unitFrom.value === "" || unitTo.value === "") {
+convertButton.addEventListener("click", () => {
+    if (unitValue.value === "") {
         msg.textContent = `Error: field empty`;
+        return;
     } else {
         msg.textContent = ``;
     }
 
+    const unitFromFactor = unitFactors[unitFrom.value];
+    const unitToFactor = unitFactors[unitTo.value];
+    const value = Number(unitValue.value);
+
+    const result = ((value * unitFromFactor) / unitToFactor).toFixed(4);
+
+    document.getElementById("conversionResult").textContent = `${value}${unitFrom.value} = ${result}${unitTo.value}`;
+    document.getElementById("result").style.display = "block";
+
+    // Hide content and the convert button
+    document.getElementsByClassName("converter-content")[0].style.display = "none";
+    document.getElementById("convertButton").style.display = "none";
+
+    // Display the reset button;
+    document.getElementById("resetButton").style.display = "block";
 })
